0c8e5b673f4f6800e9a106a97adcfa52076f7d48
thepowrhouse <sujit.deshpande@gmail.com>
2017-11-29 16:27:17 +0530

    Lombok Changes

--- services/library-service/src/main/java/com/fsd/core/services/libraryservice/models/Book.java
+++ services/library-service/src/main/java/com/fsd/core/services/libraryservice/models/Book.java
@@ -1,61 +1,20 @@
 package com.fsd.core.services.libraryservice.models;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Data
 @Entity
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-
     private String title;
-
     private String description;
-
     private String author;
-
     private String imageUrl;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
 }
--- services/library-service/src/main/java/com/fsd/core/services/libraryservice/models/dto/BookResponseDTO.java
+++ services/library-service/src/main/java/com/fsd/core/services/libraryservice/models/dto/BookResponseDTO.java
@@ -8,36 +8,4 @@ public class BookResponseDTO {
     private String description;
     private String author;
     private String imageUrl;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
 }
